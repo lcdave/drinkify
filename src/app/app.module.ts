b4d5c86f03a6c9187a6a35f1d398119846afef8e
Davide Lo Cascio <davide.locascio@atticode.com>
2022-02-20 18:39:24 +0100

    adds bulma, adds scss variables and basic view structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavigationComponent } from './sidenavigation/sidenavigation.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavigationComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sidenavigation/sidenavigation.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sidenavigation',
+  templateUrl: './sidenavigation.component.html',
+  styleUrls: ['./sidenavigation.component.scss'],
+})
+export class SidenavigationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
